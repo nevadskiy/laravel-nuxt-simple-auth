c1ee1f2a7d7e0f07f220292a6a8c16e0d5770b9a
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-05 00:05:24 +0200

    Add auth layout

---
+++ client/layouts/auth.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-full antialiased bg-blue-300 text-gray-800 bg-gradient">
+    <div class="mx-auto container h-full p-2 flex flex-col">
+      <nuxt class="flex-1" />
+    </div>
+  </div>
+</template>
+
+<style>
+  html,
+  body,
+  #__nuxt,
+  #__layout {
+    @apply h-full;
+  }
+
+  .bg-gradient {
+    background-image: linear-gradient(to right, theme('colors.blue.300'), theme('colors.blue.500'));
+  }
+</style>

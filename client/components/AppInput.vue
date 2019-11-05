7693506b31ffc61966a74b78ddfaaa935b176520
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-05 20:22:27 +0200

    Fix input fonts

--- client/components/AppInput.vue
+++ client/components/AppInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label :for="id" class="text-gray-600 text-sm font-bold">{{ label }}</label>
+    <label :for="id" class="text-gray-600 text-sm">{{ label }}</label>
 
     <div class="relative flex items-center">
       <input
@@ -8,7 +8,7 @@
         :value="value"
         v-bind="$attrs"
         :class="inputClasses"
-        class="w-full py-2 px-3 pr-10 border-2 border-transparent truncate bg-white text-gray-800 shadow rounded focus:outline-none focus:border-blue-500"
+        class="mt-1 w-full py-2 px-3 pr-10 font-light border-2 border-transparent truncate bg-white text-gray-800 shadow rounded focus:outline-none focus:border-blue-400"
         @input="update"
       >
 
@@ -21,8 +21,6 @@
 
 <script>
 export default {
-  // class="w-full py-2 px-3 border-2 border-gray-300 bg-gray-300 text-gray-800 rounded focus:outline-none focus:border-blue-400 focus:bg-transparent"
-
   inheritAttrs: false,
 
   props: {
@@ -50,11 +48,6 @@ export default {
   computed: {
     hasError () {
       return this.errors.length
-    },
-
-    inputClasses () {
-      // return this.hasError ? 'border-red-600' : 'border-gray-400'
-      return ''
     }
   },
 

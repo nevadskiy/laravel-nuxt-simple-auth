73c25f18c871203049bcace380c210725771d66b
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-18 00:26:27 +0200

    Feature button loader

--- client/components/AppButton.vue
+++ client/components/AppButton.vue
@@ -2,24 +2,40 @@
   <component
     :is="tag"
     :to="to"
-    class="inline-block py-3 px-4 rounded-full uppercase font-bold text-xs shadow-lg bg-gradient-blue text-gray-100 leading-none focus:outline-none focus:shadow-outline"
+    class="relative inline-block py-3 px-4 text-center rounded-full uppercase font-bold text-xs shadow-lg leading-none focus:outline-none focus:shadow-outline"
+    :class="classes"
     v-on="$listeners"
   >
-    <slot />
+    <div :class="{ 'opacity-0' : loading }">
+      <slot />
+    </div>
+
+    <transition name="fade">
+      <div v-if="loading" class="absolute p-1 inset-0 flex items-center justify-center">
+        <AppLoader />
+      </div>
+    </transition>
   </component>
 </template>
 
 <script>
+import AppLoader from './AppLoader.vue'
+
 export default {
+  components: {
+    AppLoader
+  },
+
   props: {
     to: {
       type: [String, Object],
       default: null
-    }
-  },
+    },
 
-  data () {
-    return {}
+    loading: {
+      type: Boolean,
+      default: false
+    }
   },
 
   computed: {
@@ -29,17 +45,51 @@ export default {
       }
 
       return 'button'
+    },
+
+    classes () {
+      return [
+        ...this.colorClasses
+      ]
+    },
+
+    colorClasses () {
+      if (this.loading) {
+        return ['bg-gradient-blue-550', 'text-gray-200']
+      }
+
+      if (this.$attrs.disabled) {
+        return ['bg-gray-500', 'text-gray-800']
+      }
+
+      return ['bg-gradient-blue-450 hover:bg-gradient-blue-550', 'text-gray-100']
     }
   }
 }
 </script>
 
-<style scoped>
-  .bg-gradient-blue {
+<style lang="scss">
+  .bg-gradient-blue-450 {
     background-image: linear-gradient(to right, theme('colors.blue.400'), theme('colors.blue.500'));
   }
 
-  .bg-gradient-blue:hover {
+  .bg-gradient-blue-550 {
     background-image: linear-gradient(to right, theme('colors.blue.500'), theme('colors.blue.600'));
+  }
+
+  .hover\:bg-gradient-blue-550 {
+    &:hover {
+      background-image: linear-gradient(to right, theme('colors.blue.500'), theme('colors.blue.600'));
+    }
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 300ms;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
   }
 </style>
---
+++ client/components/AppLoader.vue
@@ -0,0 +1,50 @@
+<template functional>
+  <svg class="loader" viewBox="25 25 50 50">
+    <circle
+      class="path stroke-current"
+      cx="50"
+      cy="50"
+      r="20"
+      fill="none"
+      stroke-width="3"
+      stroke-miterlimit="10"
+    />
+  </svg>
+</template>
+
+<style scoped>
+  .loader {
+    height: 100%;
+    width: 100%;
+    transform-origin: center center;
+    animation: rotate 2s linear infinite;
+  }
+
+  .path {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+    animation: dash 1.5s ease-in-out infinite;
+    stroke-linecap: round;
+  }
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes dash {
+    0% {
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -35px;
+    }
+    100% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -124px;
+    }
+  }
+</style>

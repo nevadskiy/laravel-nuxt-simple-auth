a4812b66696a6d671727fefee3b89cd3525e7b9c
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-24 22:30:05 +0200

    Add package service provider

--- api/packages/nevadskiy/tokens/src/TokenServiceProvider.php
+++ api/packages/nevadskiy/tokens/src/TokenServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Nevadskiy\Tokens;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Nevadskiy\Tokens\Generator\RandomHashGenerator;
 
 class TokenServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,8 @@ class TokenServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->registerConfig();
+        $this->registerDependencies();
     }
 
     /**
@@ -19,6 +22,53 @@ class TokenServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->bootMigrations();
+        $this->bootPublishable();
+    }
+
+    /**
+     * Register the application config.
+     */
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom($this->getConfigPath(), 'tokens');
+    }
+
+    /**
+     * Register any application dependencies.
+     */
+    private function registerDependencies(): void
+    {
+        $this->app->singleton(TokenManager::class);
+
+        $this->app->singletonIf(RandomHashGenerator::class, function (Application $app) {
+            return new RandomHashGenerator($app['config']['app']['key']);
+        });
+    }
+
+    /**
+     * Boot any application migrations.
+     */
+    private function bootMigrations(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+    }
+
+    /**
+     * Boot any application publishable resources.
+     */
+    private function bootPublishable(): void
+    {
+        $this->publishes([$this->getConfigPath() => config_path('tokens.php')], 'tokens-config');
+    }
+
+    /**
+     * Get the application config file path.
+     *
+     * @return string
+     */
+    private function getConfigPath(): string
+    {
+        return __DIR__ . '/../config/tokens.php';
     }
 }

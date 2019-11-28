22436e65bd8e13c3b68f8f25ddbde5aeb4223db5
Vitalii Kalaida <nevadskiy@gmail.com>
2019-11-28 22:52:53 +0200

    Add token entity

---
+++ api/packages/nevadskiy/tokens/src/TokenEntity.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Nevadskiy\Tokens;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property int tokenable_id
+ * @property string tokenable_type
+ * @property string token
+ * @property string name
+ * @property Model tokenable
+ * @property Carbon expired_at
+ * @property Carbon used_at
+ */
+class TokenEntity extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'used_at',
+        'expired_at',
+    ];
+
+    /**
+     * Create a new Eloquent model instance.
+     *
+     * @param  array  $attributes
+     * @return void
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setTable(config('tokens.table'));
+    }
+
+    /**
+     * Get tokenable model which the token is related to.
+     */
+    public function tokenable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Fill tokenable attributes according to the given model.
+     *
+     * @param Model $model
+     */
+    public function fillTokenable(Model $model): void
+    {
+        $this->tokenable_id = $model->getKey();
+        $this->tokenable_type = get_class($model);
+    }
+
+    /**
+     * Scope a query to only include tokens which belong to tokenable entity.
+     *
+     * @param Builder $query
+     * @param Model $tokenable
+     * @return Builder
+     */
+    public function scopeForTokenable(Builder $query, Model $tokenable): Builder
+    {
+        $query->where('tokenable_id', $tokenable->getKey());
+        $query->where('tokenable_type', get_class($tokenable));
+
+        return $query;
+    }
+
+    /**
+     * Scope a query to only include active tokens.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->whereNull('used_at')->where('expired_at', '>', now());
+    }
+
+    /**
+     * Continue the token expire date.
+     *
+     * @param Carbon $date
+     */
+    public function continueTo(Carbon $date): void
+    {
+        $this->update(['expired_at' => $date]);
+    }
+
+    /**
+     * Mark the token as used.
+     */
+    public function markAsUsed(): void
+    {
+        $this->update(['used_at' => now()]);
+    }
+
+    /**
+     * Determine if the token is expired already.
+     *
+     * @return bool
+     */
+    public function isExpired(): bool
+    {
+        return $this->expired_at->isPast();
+    }
+
+    /**
+     * Determine if the token is already used.
+     *
+     * @return bool
+     */
+    public function isUsed(): bool
+    {
+        return (bool) $this->used_at;
+    }
+
+    /**
+     * Convert the token entity to the string.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->token;
+    }
+}

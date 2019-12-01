ecb6ab36257354347f44ae3372ee4c9dccc17c8d
Vitalii Kalaida <nevadskiy@gmail.com>
2019-12-01 18:28:06 +0200

    Extract throttling interfaces

--- api/packages/nevadskiy/tokens/src/Token.php
+++ api/packages/nevadskiy/tokens/src/Token.php
@@ -15,6 +15,13 @@ interface Token
     public function getName(): string;
 
     /**
+     * Generate a token string.
+     *
+     * @return string
+     */
+    public function generate(): string;
+
+    /**
      * Get the token expiration date.
      *
      * @return DateInterval|DateTimeInterface|int
@@ -28,60 +35,4 @@ interface Token
      * @return string
      */
     public function getGenerationStrategy(): string;
-
-    /**
-     * Determine if the token generation throttling is enabled.
-     *
-     * @return bool
-     */
-    public function isGenerationThrottlingEnabled(): bool;
-
-    /**
-     * Get the key for identifying attempts for throttling limiter on generation process.
-     *
-     * @return string
-     */
-    public function getGenerationLimiterKey(): string;
-
-    /**
-     * Get maximum token generation attempts amount for throttling limiter
-     *
-     * @return int
-     */
-    public function getGenerationAttempts(): int;
-
-    /**
-     * Get the time interval limited generation attempts can be exhausted within.
-     *
-     * @return DateInterval|DateTimeInterface|int
-     */
-    public function getGenerationAttemptsInterval();
-
-    /**
-     * Determine if the token usage throttling is enabled.
-     *
-     * @return bool
-     */
-    public function isUsageThrottlingEnabled(): bool;
-
-    /**
-     * Get the key for identifying attempts for throttling limiter on usage process.
-     *
-     * @return string
-     */
-    public function getUsageLimiterKey(): string;
-
-    /**
-     * Get maximum token usage attempts amount for throttling limiter
-     *
-     * @return int
-     */
-    public function getUsageAttempts(): int;
-
-    /**
-     * Get the time interval limited usage attempts can be exhausted within.
-     *
-     * @return DateInterval|DateTimeInterface|int
-     */
-    public function getUsageAttemptsInterval();
 }

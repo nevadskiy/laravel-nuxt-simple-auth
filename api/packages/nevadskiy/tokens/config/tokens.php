dfd76c7750c04c82c40a89cd433e740e18ae0175
Vitalii Kalaida <nevadskiy@gmail.com>
2019-12-01 21:43:51 +0200

    Update tokens.php

--- api/packages/nevadskiy/tokens/config/tokens.php
+++ api/packages/nevadskiy/tokens/config/tokens.php
@@ -8,7 +8,7 @@ return [
     */
 
     'defined' => [
-        //'reset.password' => [
+        //'password.reset' => [
         //    'ttl' => 60,
         //    'previous' => 'remove',
         //],

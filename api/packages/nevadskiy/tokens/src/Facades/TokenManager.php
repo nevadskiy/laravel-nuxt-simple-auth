54e10b0dd33d7490a77bc4cff73c3f9a6a235fee
Vitalii Kalaida <nevadskiy@gmail.com>
2019-12-02 22:37:20 +0200

    Update facade

--- api/packages/nevadskiy/tokens/src/Facades/TokenManager.php
+++ api/packages/nevadskiy/tokens/src/Facades/TokenManager.php
@@ -12,6 +12,8 @@ use Nevadskiy\Tokens\Tokens\Token;
  * @method static TokenEntity generateFor(Model $model, Token|string $token):
  * @method static Model use(string $tokenString, Token|string $tokenType, callable $callback, Model $owner = null)
  * @method static Model useFor(string $tokenString, Token|string $tokenType, Model $owner, callable $callback)
+ * @method static void define(string $tokenName, array $options = [])
+ * @method static array getDefined()
  *
  * @see BaseTokenManager
  */

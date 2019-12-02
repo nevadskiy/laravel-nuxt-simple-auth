75659de8337feee407ce458fe31c05dfe602b86e
Vitalii Kalaida <nevadskiy@gmail.com>
2019-12-02 22:34:37 +0200

    Add TokenManager facade

---
+++ api/packages/nevadskiy/tokens/src/Facades/TokenManager.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nevadskiy\Tokens\Facades;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Facade;
+use Nevadskiy\Tokens\TokenEntity;
+use Nevadskiy\Tokens\TokenManager as BaseTokenManager;
+use Nevadskiy\Tokens\Tokens\Token;
+
+/**
+ * @method static TokenEntity generateFor(Model $model, Token|string $token):
+ * @method static Model use(string $tokenString, Token|string $tokenType, callable $callback, Model $owner = null)
+ * @method static Model useFor(string $tokenString, Token|string $tokenType, Model $owner, callable $callback)
+ *
+ * @see BaseTokenManager
+ */
+class TokenManager extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return BaseTokenManager::class;
+    }
+}

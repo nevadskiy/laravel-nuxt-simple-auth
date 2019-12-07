d8e98c0befd546fabda9d26aefa00592478164df
Vitalii Kalaida <nevadskiy@gmail.com>
2019-12-08 01:26:59 +0200

    Fix tests

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -2,27 +2,9 @@
 
 namespace Tests;
 
-use Carbon\Carbon;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    /**
-     * Freeze time.
-     *
-     * @return Carbon
-     */
-    protected function freezeTime(): Carbon
-    {
-        // Allows to use this time in comparing with database time.
-        $time = Carbon::createFromTimestamp(
-            Carbon::now()->getTimestamp()
-        );
-
-        Carbon::setTestNow($time);
-
-        return $time;
-    }
 }

fc8b76bb9d9e476466074a78cfec92f23b29edb5
Vitalii Kalaida <nevadskiy@gmail.com>
2020-01-24 00:33:35 +0200

    Refactor with table constant

--- api/modules/auth/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/modules/auth/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Module\Auth\Models\User;
 
 class CreateUsersTable extends Migration
 {
@@ -13,7 +14,7 @@ class CreateUsersTable extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create(User::TABLE, function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists(User::TABLE);
     }
 }
